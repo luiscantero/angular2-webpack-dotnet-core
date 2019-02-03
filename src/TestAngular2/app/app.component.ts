72e0ccc89d2e827dd15b96f4ef2c153e42c18b0e
Luis Cantero <luiscantero@hotmail.com>
2019-02-03 20:18:09 +0100

    Moved adal config to environment file

--- src/TestAngular2/app/app.component.ts
+++ src/TestAngular2/app/app.component.ts
@@ -4,16 +4,7 @@ import { NgRedux } from '@angular-redux/store';
 import { IAppState, reducer, INITIAL_STATE } from './store';
 
 import { AdalService } from 'adal-angular4';
-
-const adalConfig: adal.Config = {
-    tenant: '', // Azure AD tenant: <TENANT>.onmicrosoft.com
-    clientId: '00000000-0000-0000-0000-000000000000', // Azure App ID (App Registration).
-    endpoints: {
-        //'<URL_TO_BE_GUARDED_BY_INTERCEPTION>': '<AAD_APP_ID>'
-        'https://graph.microsoft.com': '00000003-0000-0000-c000-000000000000',
-        //'http://localhost:8081/api/authors': '00000000-0000-0000-0000-000000000000',
-    },
-};
+import { environment } from '../environments/environment';
 
 @Component({
     selector: 'my-app',
@@ -27,8 +18,8 @@ export class AppComponent {
         private adalService: AdalService) {
         ngRedux.configureStore(reducer, INITIAL_STATE); // Call once for lifetime of app.
 
-        if (adalConfig.tenant.length > 0) {
-            this.adalService.init(adalConfig);
+        if (environment.adalConfig.tenant.length > 0) {
+            this.adalService.init(environment.adalConfig);
         }
     }
 
--- src/TestAngular2/environments/environment.ts
+++ src/TestAngular2/environments/environment.ts
@@ -1,4 +1,13 @@
 export const environment = {
     production: false,
     authorsUrl: "http://localhost:8081/api/authors",
+    adalConfig: <adal.Config> {
+        tenant: '', // Azure AD tenant: <TENANT>.onmicrosoft.com
+        clientId: '00000000-0000-0000-0000-000000000000', // Azure App ID (App Registration).
+        endpoints: {
+            //'<URL_TO_BE_GUARDED_BY_INTERCEPTION>': '<AAD_APP_ID>'
+            'https://graph.microsoft.com': '00000003-0000-0000-c000-000000000000',
+            //'http://localhost:8081/api/authors': '00000000-0000-0000-0000-000000000000',
+        },
+    },
 };
